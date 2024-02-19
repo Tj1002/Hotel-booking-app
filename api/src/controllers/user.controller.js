7a31ee4bb4b5e08180943f4434e6184b3c15cb2e
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-20 00:07:42 +0530

    adding logout functionality

--- api/src/controllers/user.controller.js
+++ api/src/controllers/user.controller.js
@@ -88,5 +88,29 @@ const registerUser = asyncHandler( async (req, res) => {
     return res.status(201).json(
         new ApiResponse(200, createdUser, "User registered Successfully")
     )
-} )
-export {registerUser,loginUser}
+})
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $unset: {
+        refreshToken: 1, 
+      },
+    },
+    {
+      new: true,
+    }
+  );
+
+  const options = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", options)
+    .clearCookie("refreshToken", options)
+    .json(new ApiResponse(200, {}, "User logged Out"));
+});
+export {registerUser,loginUser,logoutUser}
--- api/src/routes/user.routes.js
+++ api/src/routes/user.routes.js
@@ -1,7 +1,9 @@
 import express from "express"
-import { loginUser, registerUser } from "../controllers/user.controller.js"
+import { loginUser, logoutUser, registerUser } from "../controllers/user.controller.js"
+import { verifyJWT } from "../middlewares/user.middleware.js"
 const router=express.Router()
 
 router.route('/register').post(registerUser)
 router.route('/login').post(loginUser)
+router.route('/logout').post(verifyJWT,logoutUser)
 export default router
