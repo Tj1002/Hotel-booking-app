e406b73575b5f7c0e393491b608c360facea030e
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-20 12:09:40 +0530

    adding update and delete hotel functionality

--- api/src/controllers/my-hotel.controller.js
+++ api/src/controllers/my-hotel.controller.js
@@ -90,4 +90,87 @@ const getSingleHotel=asyncHandler(async(req,res)=>{
     .status(201)
     .json(new ApiResponse(200, hotel, "fetching hotel Successfully"));
 })
-export {registerHotel,getAllHotels,getSingleHotel}
+const updateHotel=asyncHandler(async(req,res)=>{
+  const userId = req.user._id;
+  if (!userId) {
+    throw new ApiError(400, "You are not authroized to view this");
+  }
+  const id = req.params.id
+  const {
+    name,
+    city,
+    country,
+    description,
+    type,
+    adultCount,
+    childCount,
+    facilities,
+    pricePerNight,
+    starRating,
+  } = req.body;
+  if (
+    [
+      name,
+      city,
+      country,
+      description,
+      type,
+      adultCount,
+      childCount,
+      facilities,
+      pricePerNight,
+      starRating,
+    ].some((field) => field?.trim() === "")
+  ) {
+    throw new ApiError(400, "All fields are required");
+  }
+  const files=req.files
+  console.log("files",files);
+  const imageUrlLocalPath = files && files.map((file) => file.path);
+  console.log("imageurllocalpath", imageUrlLocalPath);
+  const updatedImageUrls = await uploadOnCloudinary(imageUrlLocalPath);
+  const frontendImageUrls= req.body.imageUrls || []
+  const allImageUrls= [...updatedImageUrls,...frontendImageUrls]
+  if(allImageUrls.length>6){
+    throw new ApiError(400, "maximum 6 images are allowed");
+  }
+  const updatedHotel = await Hotel.findByIdAndUpdate({_id:id}, {
+    $set: {
+      userId,
+      name,
+      city,
+      country,
+      description,
+      type,
+      adultCount,
+      childCount,
+      facilities,
+      pricePerNight,
+      starRating,
+      imageUrls: allImageUrls && allImageUrls.map((imageUrl) => imageUrl.url),
+      lastUpdated: new Date(),
+    },
+  },{
+    new:true
+  })
+  if(!updatedHotel){
+    throw new ApiError(500, "Error while updating hotels");
+  }
+   return res
+     .status(201)
+     .json(new ApiResponse(200, updatedHotel, "hotel updating Successfully"));
+})
+const deleteHotel=asyncHandler(async(req,res)=>{
+  const userId = req.user._id;
+  if (!userId) {
+    throw new ApiError(400, "You are not authroized to view this");
+  }
+  const id= req.params.id
+  await Hotel.findByIdAndDelete({
+    _id:id
+  })
+  return res
+    .status(201)
+    .json(new ApiResponse(200,"fetching Deleted Successfully"));
+})
+export {registerHotel,getAllHotels,getSingleHotel,updateHotel,deleteHotel}
--- api/src/routes/my-hotel.routes.js
+++ api/src/routes/my-hotel.routes.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { verifyJWT } from "../middlewares/user.middleware.js";
 import { upload } from "../middlewares/multer.middleware.js";
-import { getAllHotels, getSingleHotel, registerHotel } from "../controllers/my-hotel.controller.js";
+import { deleteHotel, getAllHotels, getSingleHotel, registerHotel, updateHotel } from "../controllers/my-hotel.controller.js";
 const router = express.Router();
 router.route("/register").post(verifyJWT,upload.array("imageFiles",6),registerHotel);
 router.route('/getAllHotels').get(verifyJWT,getAllHotels)
 router.route('/getSingleHotel/:id').get(verifyJWT,getSingleHotel)
+router
+  .route("/updateHotel/:id")
+  .put(verifyJWT, upload.array("imageFiles"), updateHotel);
+
+router.route("/deleteHotel/:id").delete(verifyJWT, deleteHotel);
+
 export default router
