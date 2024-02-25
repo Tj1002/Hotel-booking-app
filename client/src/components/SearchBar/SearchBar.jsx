556ad4123496c1390c40782f1925ef2c86925457
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-25 13:39:45 +0530

    adding search bar

---
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,111 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  updateDestination,
+  updateCheckIn,
+  updateCheckOut,
+  updateAdultCount,
+  updateChildCount,
+} from "./../../redux/features/searchSlice";
+import { MdTravelExplore } from "react-icons/md";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { useNavigate } from "react-router-dom";
+
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const [destination, setDestination] = useState("");
+  const [checkIn, setCheckIn] = useState(new Date());
+  const [checkOut, setCheckOut] = useState(new Date());
+  const [adultCount, setAdultCount] = useState(1);
+  const [childCount, setChildCount] = useState(0);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(updateDestination(destination));
+    dispatch(updateCheckIn(checkIn));
+    dispatch(updateCheckOut(checkOut));
+    dispatch(updateAdultCount(adultCount));
+    dispatch(updateChildCount(childCount));
+    navigate("/search");
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="-mt-8 p-3 bg-[#b46617] rounded shadow-md grid grid-cols-2 lg:grid-cols-3 2xl:grid-cols-5 items-center gap-4"
+    >
+      <div className="flex flex-row items-center flex-1 bg-white p-2">
+        <MdTravelExplore size={25} className="mr-2" />
+        <input
+          placeholder="Where are you going?"
+          className="text-md w-full focus:outline-none"
+          value={destination}
+          onChange={(event) => setDestination(event.target.value)}
+        />
+      </div>
+
+      <div className="flex bg-white px-2 py-1 gap-2">
+        <label className="items-center flex">
+          Adults:
+          <input
+            className="w-full p-1 focus:outline-none font-bold"
+            type="number"
+            min={1}
+            max={20}
+            value={adultCount}
+            onChange={(event) => setAdultCount(parseInt(event.target.value))}
+          />
+        </label>
+        <label className="items-center flex">
+          Children:
+          <input
+            className="w-full p-1 focus:outline-none font-bold"
+            type="number"
+            min={0}
+            max={20}
+            value={childCount}
+            onChange={(event) => setChildCount(parseInt(event.target.value))}
+          />
+        </label>
+      </div>
+      <div>
+        <DatePicker
+          selected={checkIn}
+          onChange={(date) => setCheckIn(date)}
+          selectsStart
+          startDate={checkIn}
+          endDate={checkOut}
+          minDate={new Date()}
+          placeholderText="Check-in Date"
+          className="min-w-full bg-white p-2 focus:outline-none flex flex-shrink"
+          wrapperClassName="min-w-full"
+        />
+      </div>
+      <div>
+        <DatePicker
+          selected={checkOut}
+          onChange={(date) => setCheckOut(date)}
+          selectsStart
+          startDate={checkIn}
+          endDate={checkOut}
+          minDate={checkIn}
+          placeholderText="Check-out Date"
+          className="min-w-full bg-white p-2 focus:outline-none"
+          wrapperClassName="min-w-full"
+        />
+      </div>
+      <div className="flex gap-1">
+        <button className="w-2/3 bg-[#ffba00] text-white h-full p-2 font-bold text-xl hover:bg-[#ffba00]">
+          Search
+        </button>
+        <button className="w-1/3 bg-red-600 text-white h-full p-2 font-bold text-xl hover:bg-red-500">
+          Clear
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default SearchBar;
