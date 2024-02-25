3310b96b56fe3c10b6ce7db145590f4c1f4346cc
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-25 11:53:28 +0530

    Home functionality added

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,45 @@
+import { useEffect, useState } from "react";
 
+import LatestDestinationCard from "../components/DestinationCard/LatestDestinationCard";
 
-function Home() {
+function HotelDetails() {
+  const [hotels, setHotels] = useState([]);
+
+  useEffect(() => {
+    const getAllHotel = async () => {
+      try {
+        const response = await fetch(`/api/v1/my-hotels/getAllHotels`);
+        const result = await response.json();
+        const hotel = result.data;
+        console.log(hotel);
+        setHotels(hotel);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getAllHotel();
+  }, []);
+  const topRowHotels = hotels?.slice(0, 2) || [];
+  const bottomRowHotels = hotels?.slice(2) || [];
+
   return (
-    <div>
-      register
+    <div className="space-y-3">
+      <h2 className="text-3xl font-bold">Latest Destinations</h2>
+      <p>Most recent desinations added by our hosts</p>
+      <div className="grid gap-4">
+        <div className="grid md:grid-cols-2 grid-cols-1 gap-4">
+          {topRowHotels.map((hotel) => (
+            <LatestDestinationCard hotel={hotel} key={hotel._id} />
+          ))}
+        </div>
+        <div className="grid md:grid-cols-3 gap-4">
+          {bottomRowHotels.map((hotel) => (
+            <LatestDestinationCard hotel={hotel} key={hotel._id}/>
+          ))}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default HotelDetails;
