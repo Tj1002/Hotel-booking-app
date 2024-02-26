f4bb28db7d231ac4e7a5513738a801686ded362a
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-27 00:01:42 +0530

    pre installation

--- api/src/app.js
+++ api/src/app.js
@@ -1,28 +1,39 @@
-import express from "express"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import morgan from "morgan"
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import path from "path";
 
-const app = express()
-app.use(morgan('tiny'))
-app.use(cors({
+const __dirname = path.resolve();
+app.use(express.static(path.join(__dirname, "/client/dist")));
+
+const app = express();
+app.use(morgan("tiny"));
+app.use(
+  cors({
     origin: process.env.CORS_ORIGIN,
-    credentials: true
-}))
+    credentials: true,
+  })
+);
 
-app.use(express.json({limit: "16kb"}))
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
-app.use(express.static("public"))
-app.use(cookieParser())
+app.use(express.json({ limit: "16kb" }));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
+
+app.use(cookieParser());
 //importing router
-import userRouter from "./routes/user.routes.js"
-import myHotelRouter from "./routes/my-hotel.routes.js"
-import hotelRouter from "./routes/hotel.routes.js"
-import bookingRouter from "./routes/booking.routes.js"
+import userRouter from "./routes/user.routes.js";
+import myHotelRouter from "./routes/my-hotel.routes.js";
+import hotelRouter from "./routes/hotel.routes.js";
+import bookingRouter from "./routes/booking.routes.js";
 
-app.use('/api/v1/users',userRouter)
-app.use('/api/v1/my-hotels',myHotelRouter)
-app.use('/api/v1/hotels',hotelRouter)
-app.use('/api/v1/bookings',bookingRouter)
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/my-hotels", myHotelRouter);
+app.use("/api/v1/hotels", hotelRouter);
+app.use("/api/v1/bookings", bookingRouter);
 
-export default app
+app.use(express.static(path.join(__dirname, "/client/dist")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "dist", "index.html"));
+});
+
+export default app;
