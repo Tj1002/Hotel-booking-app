67a5936ac0aff810caef2403c443e328a29bb0be
Md Tausif Jawed <tausifjawed1002@gmail.com>
2024-02-26 13:12:33 +0530

    Booking page added

---
+++ client/src/pages/Booking.jsx
@@ -0,0 +1,65 @@
+import BookingForm from "../forms/BookingForm/BookingForm";
+
+import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BookingDetailsSummary from "../components/BookingDetailsSummary";
+import { Elements } from "@stripe/react-stripe-js";
+import { useSelector } from "react-redux";
+
+const Booking = () => {
+  const search = useSelector((state) => state.search);
+  const { currentUser } = useSelector((state) => state.user);
+  const { stripePromise } = useAppContext();
+
+  const { hotelId } = useParams();
+
+  const [numberOfNights, setNumberOfNights] = useState(0);
+
+  useEffect(() => {
+    if (search.checkIn && search.checkOut) {
+      const nights =
+        Math.abs(search.checkOut.getTime() - search.checkIn.getTime()) /
+        (1000 * 60 * 60 * 24);
+
+      setNumberOfNights(Math.ceil(nights));
+    }
+  }, [search.checkIn, search.checkOut]);
+
+  //payment intent
+
+  //search by id
+
+  // current user fetch
+
+  if (!hotel) {
+    return <></>;
+  }
+
+  return (
+    <div className="grid md:grid-cols-[1fr_2fr]">
+      <BookingDetailsSummary
+        checkIn={search.checkIn}
+        checkOut={search.checkOut}
+        adultCount={search.adultCount}
+        childCount={search.childCount}
+        numberOfNights={numberOfNights}
+        hotel={hotel}
+      />
+      {currentUser && paymentIntentData && (
+        <Elements
+          stripe={stripePromise}
+          options={{
+            clientSecret: paymentIntentData.clientSecret,
+          }}
+        >
+          <BookingForm
+            currentUser={currentUser}
+            paymentIntent={paymentIntentData}
+          />
+        </Elements>
+      )}
+    </div>
+  );
+};
+
+export default Booking;
